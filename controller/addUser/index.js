2f0d7cc9503e72e7ff52617c26884dc00ee515fd
LWANG267 <LWANG267@ford.com>
2022-12-27 17:06:11 +0800

    真题，模拟，密卷开发

--- controller/addUser/index.js
+++ controller/addUser/index.js
@@ -1,19 +1,4 @@
-const getTime = () => {
-  let date = new Date();
-  let YY = date.getFullYear() + "-";
-  let MM =
-    (date.getMonth() + 1 < 10
-      ? "0" + (date.getMonth() + 1)
-      : date.getMonth() + 1) + "-";
-  let DD = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
-  let hh =
-    (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":";
-  let mm =
-    (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()) +
-    ":";
-  let ss = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
-  return YY + MM + DD + " " + hh + mm + ss;
-};
+let { getTime } = require("../../utils/getTime");
 
 module.exports = function (app, connection) {
   // 登录接口
--- controller/denseVolume/index.js
+++ controller/denseVolume/index.js
@@ -1,10 +1,10 @@
 let { getTime } = require("../../utils/getTime");
 module.exports = function (app, connection) {
-  // 获取章节列表
-  app.post("/chaptersList", function (req, res) {
+  // 获取密卷列表
+  app.post("/denseVolumeList", function (req, res) {
     let grade = req.body.grade;
     connection.query(
-      `select * from Chapters where grade='${grade}';`,
+      `select * from DenseVolume where grade='${grade}';`,
       function (err, results) {
         return res.send({
           status: 200,
@@ -15,12 +15,12 @@ module.exports = function (app, connection) {
     );
   });
 
-  // 添加章节
-  app.post("/addChapter", function (req, res) {
+  // 添加密卷
+  app.post("/addDenseVolume", function (req, res) {
     let grade = req.body.grade;
     let name = req.body.name;
     connection.query(
-      `INSERT INTO Chapters(create_time,grade,name) VALUES('${getTime()}','${grade}','${name}');`,
+      `INSERT INTO DenseVolume(create_time,grade,name) VALUES('${getTime()}','${grade}','${name}');`,
       function (err, results) {
         return res.send({
           status: 200,
@@ -31,12 +31,12 @@ module.exports = function (app, connection) {
     );
   });
 
-  // 编辑章节名称
-  app.post("/editChapterName", function (req, res) {
+  // 编辑密卷名称
+  app.post("/editDenseVolumeName", function (req, res) {
     let id = req.body.id;
     let name = req.body.name;
     connection.query(
-      `update Chapters set name='${name}' where id='${id}';`,
+      `update DenseVolume set name='${name}' where id='${id}';`,
       function (err, results) {
         return res.send({
           status: 200,
@@ -47,11 +47,11 @@ module.exports = function (app, connection) {
     );
   });
 
-  // 获取章节下的题目
-  app.post("/queryChapterQuestions", function (req, res) {
-    let zjid = req.body.zjid;
+  // 获取密卷下的题目
+  app.post("/queryDenseVolumeQuestions", function (req, res) {
+    let mjid = req.body.mjid;
     connection.query(
-      `select * from zjtm where zjid='${zjid}';`,
+      `select * from mjtm where mjid='${mjid}';`,
       function (err, results) {
         return res.send({
           status: 200,
@@ -62,8 +62,8 @@ module.exports = function (app, connection) {
     );
   });
 
-  // 编辑单个题目
-  app.post("/editQuestion", function (req, res) {
+  // 编辑密卷单个题目
+  app.post("/editDenseVolumesQuestion", function (req, res) {
     let tmid = req.body.tmid;
     let analysis = req.body.analysis;
     let answer = req.body.answer;
@@ -71,7 +71,7 @@ module.exports = function (app, connection) {
     let title = req.body.title;
     let type = req.body.type;
     connection.query(
-      `update zjtm set analysis='${analysis}',answer='${answer}',type='${type}',options='${options}',title='${title}' where tmid='${tmid}';`,
+      `update mjtm set analysis='${analysis}',answer='${answer}',type='${type}',options='${options}',title='${title}' where tmid='${tmid}';`,
       function (err, results) {
         return res.send({
           status: 200,
@@ -82,11 +82,11 @@ module.exports = function (app, connection) {
     );
   });
 
-  // 删除单个题目
-  app.post("/deleteQuestion", function (req, res) {
+  // 删除单个密卷题目
+  app.post("/deleteDenseVolumesQuestion", function (req, res) {
     let tmid = req.body.tmid;
     connection.query(
-      `DELETE FROM zjtm  where tmid='${tmid}'`,
+      `DELETE FROM mjtm  where tmid='${tmid}'`,
       function (err, results) {
         return res.send({
           status: 200,
@@ -97,11 +97,11 @@ module.exports = function (app, connection) {
     );
   });
 
-  // 删除单个章节及章节下的题目
-  app.post("/deleteChapterAndQuestion", function (req, res) {
+  // 删除单个密卷及密卷下的题目
+  app.post("/deleteDenseVolumesAndQuestion", function (req, res) {
     let id = req.body.id;
     connection.query(
-      `DELETE FROM Chapters  where id='${id}';DELETE FROM zjtm  where zjid='${id}';`,
+      `DELETE FROM DenseVolume  where id='${id}';DELETE FROM mjtm  where mjid='${id}';`,
       function (err, results) {
         return res.send({
           status: 200,
@@ -112,18 +112,18 @@ module.exports = function (app, connection) {
     );
   });
 
-  // 同一章节批量导入题目
-  app.post("/addQuestions", function (req, res) {
+  // 同一密卷批量导入题目
+  app.post("/addDenseVolumesQuestions", function (req, res) {
     let sql = '';
     req.body.map(item => {
-      let zjid = item.zjid;
+      let mjid = item.mjid;
       let analysis = item.analysis;
       let answer = item.answer;
       let options = item.option;
       let title = item.title;
       let type = item.type;
       let create_time = getTime();
-      sql += `INSERT INTO zjtm SET zjid='${zjid}',analysis='${analysis}',answer='${answer}',options='${options}',title='${title}',type='${type}',create_time='${create_time}';`
+      sql += `INSERT INTO mjtm SET mjid='${mjid}',analysis='${analysis}',answer='${answer}',options='${options}',title='${title}',type='${type}',create_time='${create_time}';`
     })
     connection.query(sql ,function (err, results) {
         return res.send({
